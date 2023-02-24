52518e4dc0c2110e342bd05e2d0370b6f19cdae4
bscoderuz <mrbscoder@gmail.com>
2023-02-24 02:47:37 -0800

    add detail page

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import news_list
+from .views import news_list, news_detail
 
 urlpatterns = [
-    path('all/', news_list, name='news_list')
+    path('all/', news_list, name='news_list'),
+    path('<int:id>/', news_detail, name='news_detail'),
 ]
