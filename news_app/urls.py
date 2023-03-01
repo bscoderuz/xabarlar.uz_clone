1b680b564a9c38c5dac9acc27d828ed1eaf4133a
bscoderuz <mrbscoder@gmail.com>
2023-02-28 22:44:22 -0800

    add delete and update class

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from .views import news_list, news_detail, home, contact, notFoundPage, HomePageView, LocalNewsView, EuroNewsView, \
-    TexnoNewsView, SportNewsView
+    TexnoNewsView, SportNewsView, NewsUpdateView, NewsDeleteView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('news/', news_list, name='news_list'),
     path('news/<slug:news>/', news_detail, name='news_detail'),
+    path('news/<slug>/edit/', NewsUpdateView.as_view(), name='news_edit'),
+    path('news/<slug>/delete/', NewsDeleteView.as_view(), name='news_delete'),
     path('local-news/', LocalNewsView.as_view(), name='local-news'),
     path('euro-news/', EuroNewsView.as_view(), name='euro-news'),
     path('sport-news/', SportNewsView.as_view(), name='sport-news'),
     path('texno-news/', TexnoNewsView.as_view(), name='texno-news'),
     path('not-found/', notFoundPage, name='not_found'),
-    path('contact', contact, name='contact_page'),
+    path('contact/', contact, name='contact_page'),
 
 ]
--- news_app/views.py
+++ news_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, ListView
-
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 from news_app.models import News, Category, Contact
 from .forms import ContactForm
 
@@ -25,6 +25,7 @@ def news_detail(request, news):
     return render(request, 'news/news_detail.html', context)
 
 
+# function base view
 def home(request):
     categories = Category.objects.all()
     news_list = News.published.all().order_by('-publish_time')[:10]
@@ -39,6 +40,7 @@ def home(request):
     return render(request, 'news/home.html', context)
 
 
+# class base view
 class HomePageView(ListView):
     model = News
     template_name = 'news/home.html'
@@ -96,7 +98,7 @@ def notFoundPage(request):
 class LocalNewsView(ListView):
     model = News
     template_name = 'news/local-news.html'
-    context_object_name = 'local-news'
+    context_object_name = 'local_news'
 
     def get_queryset(self):
         news = self.model.published.all().filter(category__name="Mahalliy")
@@ -106,25 +108,40 @@ class LocalNewsView(ListView):
 class EuroNewsView(ListView):
     model = News
     template_name = 'news/euro-news.html'
-    context_object_name = 'euro-news'
+    context_object_name = 'euro_news'
 
     def get_queryset(self):
         news = self.model.published.all().filter(category__name="Xorij")
+        return news
 
 
 class TexnoNewsView(ListView):
     model = News
     template_name = 'news/texno-news.html'
-    context_object_name = 'texno-news'
+    context_object_name = 'texno_news'
 
     def get_queryset(self):
         news = self.model.published.all().filter(category__name="Texnologiya")
+        return news
 
 
 class SportNewsView(ListView):
     model = News
     template_name = 'news/sport-news.html'
-    context_object_name = 'sport-news'
+    context_object_name = 'sport_news'
 
     def get_queryset(self):
         news = self.model.published.all().filter(category__name="Sport")
+        return news
+
+
+class NewsUpdateView(UpdateView):
+    model = News
+    fields = ('title', 'body', 'image', 'category', 'status',)
+    template_name = 'crud/news-edit.html'
+
+
+class NewsDeleteView(DeleteView):
+    model = News
+    template_name = 'crud/news-delete.html'
+    success_url = reverse_lazy('home_page')
